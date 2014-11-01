af3b01f7724e0e7784d019163892d6b14c4e5c86
Antoine EISELE <Antoine_eisele@hotmail.com>
2014-11-01 12:58:44 +0100

    Ajout de la page de login et modifications des articles (+ Modification des liens)

--- index.php
+++ index.php
@@ -4,28 +4,36 @@
  * This code is under MIT licence (see https://github.com/Irvyne/license/blob/master/MIT.md)
  */
 
-require __DIR__.'/_header.php';
+require __DIR__ . '/_header.php';
 
 $perPage = 6; // nbArticleParPage
 $nbArticles = countArticles($link); //nbArticleTotal
 $currentPage = !empty($_GET['p']) ? (int)$_GET['p'] : 1;// numéro de la page
-$nbPages = ceil($nbArticles/$perPage); // nombre de pagination
+$nbPages = ceil($nbArticles / $perPage); // nombre de pagination
 
+
+if (isConnected()) {
+    $username = $_SESSION['username'];
+} else {
+    $username = false;
+}
 
 if (0 >= $currentPage) {
     header('Location: index.php?p=1');
 }
 if ($currentPage > $nbPages) {
-    header('Location: index.php?p='.$nbPages);
+    header('Location: index.php?p=' . $nbPages);
 }
 
-$articles = getArticles($link, null, ($currentPage-1)*$perPage, $perPage);
+$articles = getArticles($link, null, ($currentPage - 1) * $perPage, $perPage);
 
 echo $twig->render('articles.html.twig', [
     'articles' => $articles,
+    'perpage' => $perPage,
+    'currentPage' => $currentPage,
+    'nbPages' => $nbPages,
     'connected' => isConnected(),
-    'username' => 'Admin',
-
+    'username' => $username,
 ]);
 
-require __DIR__.'/_footer.php';
+require __DIR__ . '/_footer.php';
